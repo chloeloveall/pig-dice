2d39016f05ccb7824a907b6eeb8e8ffceb17f3cf
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-28 15:51:53 -0800

    Refactor prototype.hol and prototype.addRound, add branching statement to prototpe.hold
    Co-authored-by: Alan Calll<alanmatthewcall@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,51 @@
+// Business Logic for Game ---------
 function Game() {
   this.players = {};
   // this.gameOver = true;
 }
+
 Game.prototype.addPlayer = function(player) {
   this.players[player.playerName] = player;
 }
+
+// Business Logic for Player ---------
 function Player(playerName) {
   this.playerName = playerName;
   this.roundScore = 0;
-  this.totalScore = 0;
+  this.totalScore = 100;
 }
+
+Player.prototype.hold = function () {
+  this.totalScore = this.roundScore + this.totalScore;
+  if (this.totalScore >= 100) {
+    return "Score total: " + this.totalScore + " CONGRATS YOU ARE THE WINNER!";
+  } else if (this.totalScore < 100) {
+    alert("It is now the other player's turn");
+    return this.totalScore;
+  } 
+}
+
 Player.prototype.rollDie = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
-Player.prototype.playerRoll = function() {
+
+Player.prototype.addRound = function() {
   const currentRound = this.rollDie();
   if (currentRound === 1) {
-    console.log('this is telling you the score is zero')
     return this.roundScore = 0;
   } else {
-    return this.roundScore += currentRound;
+    return this.roundScore = this.roundScore + currentRound;
   }
 };
 
+let newGame = new Game();
+let player1 = new Player("Chloe");
+let player2 = new Player('Alan');
+newGame.addPlayer(player1);
+newGame.addPlayer(player2);
+player1.hold();
 
-let newPlayer = new Player("Chloe");
+// User Interface Logic 
 
 
 
