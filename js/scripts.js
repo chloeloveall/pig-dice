b184ed8b89caa88a822e8406ceccec28eff5fb98
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-28 14:03:04 -0800

    Pass TDD test to add player to game object with prototype.addPlayer
    Co-authored-by: Alan Calll<alanmatthewcall@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
-
-function Game(player) {
-  this.player = player;
-  this.gameOver = true;
+function Game() {
+  this.players = {};
+  // this.gameOver = true;
 }
-
-
+Game.prototype.addPlayer = function(player) {
+  this.players[player.playerName] = player;
+}
 function Player(playerName) {
   this.playerName = playerName;
   this.roundScore = 0;
   this.totalScore = 0;
 }
-
 Player.prototype.rollDie = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
+Player.prototype.playerRoll = function() {
+  const currentRound = this.rollDie();
+  if (currentRound === 1) {
+    console.log('this is telling you the score is zero')
+    return this.roundScore = 0;
+  } else {
+    return this.roundScore += currentRound;
+  }
+};
 
 
+let newPlayer = new Player("Chloe");
 
-// Player.prototype.addRound = function() {
-//   return this.roundScore += rollDie;
-// };
-
-// let newPlayerInstance = new Player("Chloe", 80, 100);
 
 
